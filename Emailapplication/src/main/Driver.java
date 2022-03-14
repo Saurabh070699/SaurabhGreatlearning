9db01bd0bdc206a00eba324b5045d860bec115bf
Saurabh070699 <89467323+Saurabh070699@users.noreply.github.com>
2022-03-14 20:55:01 +0530

    Add files via upload

---
+++ Emailapplication/src/main/Driver.java
@@ -0,0 +1,57 @@
+package main;
+import java.util.Scanner;
+import Emailapplication.Employee;
+import Credentials;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee("firstname", "lastname");
+		Credentials cs = new Credentials();
+		String generatedEmail;
+		char[] generatedPassword;
+
+		System.out.println("Please enter the department from the following ");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+
+		if (option == 1) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "tech");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else if (option == 2) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "adm");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else if (option == 3) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "hr");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+
+		else if (option == 4) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "lg");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else
+			System.out.println("Enter a valid option");
+		
+		sc.close();
+	}
+}
