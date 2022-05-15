987ab7d78d1ad4241d98f5f5ba1f2e430c079bad
Saurabh070699 <89467323+Saurabh070699@users.noreply.github.com>
2022-05-15 22:59:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package DataStructureAssignmentSolution.Construction;
+
+public class Main {
+    public static void main(String[] args) {
+        Construction construction = new Construction();
+        construction.getIData();
+        construction.orderCalculator(construction.floorSize);
+    }
+}
