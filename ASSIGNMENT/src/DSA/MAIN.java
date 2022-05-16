de7b9740236752fb5bbf0f976159b1057fb0f746
Saurabh070699 <89467323+Saurabh070699@users.noreply.github.com>
2022-05-16 10:16:03 +0530

    Add files via upload

---
+++ ASSIGNMENT/src/DSA/MAIN.java
@@ -0,0 +1,9 @@
+package DSA;
+
+public class MAIN {
+	public static void main(String[] args) {
+        CONSTRUCTION construction = new CONSTRUCTION();
+        construction.getIData();
+        construction.orderCalculator(construction.floorSize);
+    }
+}
